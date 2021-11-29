b8c36bbf793dce4617686df4c80a2c75a65f84f7
Pawspanda <91129152+Pawspanda@users.noreply.github.com>
2021-11-29 18:52:19 +0200

    defined in views: a course with shown homeworks

--- homeworkapp/views.py
+++ homeworkapp/views.py
@@ -6,8 +6,15 @@ def index(request):
     """The home page of the Course"""
     return render(request, 'homeworkapp/index.html')
 
-def course(request):
+def courses(request):
     """show all homeworks."""
     courses = Course.objects.order_by('date_added')
     context = {'courses': courses}
     return render(request, 'homeworkapp/courses.html', context)
+
+def course(request, course_id):
+    """Shows the individual course and all its homeworks."""
+    course = Course.objects.get(id=course_id)
+    homeoworks = course.homework_set.order_by('-date_added')
+    context = {'course': course, 'homeoworks': homeoworks}
+    return render(request, 'homeworkapp/course.html', context)
