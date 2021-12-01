d980b1eeca920341a3069614b971ffd8be3d8099
Iiris Koivisto <koivisto.irkku@gmail.com>
2021-12-01 12:04:48 +0200

    user/urls.py includes registration path

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,12 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 url_patterns = [
     #Include default auth paths
     path('', include('django.contrib.auth.urls')),
+    #Registration page
+    path('register/', views.register, name='register'),
 ]
