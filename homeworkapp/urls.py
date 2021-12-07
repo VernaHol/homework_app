fb21c6046b442352ae397a710fb610bdb44c4c98
Solea <soleafoly@gmail.com>
2021-12-07 11:34:42 +0200

    Added new homework and new course to the urls file

--- homeworkapp/urls.py
+++ homeworkapp/urls.py
@@ -11,5 +11,8 @@ urlpatterns = [
     path('courses/', views.courses, name='courses'),
     #Page that shows a certain courses homework
     path('course/<int:course_id>', views.course, name='course'),
-    #Page that shows the homework added
+    #Page for adding a new course
+    path('new_course/', views.new_course, name='new_course'),
+    #Path for adding a new homework
+    path('new_homework/', views.new_homework, name='new_homework'),
 ]
