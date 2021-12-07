eba9027c6c1a7b9dc494e9f1bf2636480a120064
Solea <soleafoly@gmail.com>
2021-12-07 12:16:44 +0200

    Added a view for a new course

--- homeworkapp/views.py
+++ homeworkapp/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Course, Homework
+from .forms import CourseForm, HomeworkForm
+
 
 # Create your views here.
 
@@ -18,3 +22,23 @@ def course(request, course_id):
     homeoworks = course.homework_set.order_by('-date_added')
     context = {'course': course, 'homeoworks': homeoworks}
     return render(request, 'homeworkapp/course.html', context)
+
+def new_course(request):
+    """Add a new course"""
+    if request.method != 'POST':
+        #No data submitted; create a blank form.
+        form = CourseForm()
+    else:
+        #POST data submitted; process data.
+        form = CourseForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('homeworkapp:topics')
+
+    #Displays a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'homeworkapp/new_course.html', context)
+
+
+
+            
